6ade979d175688ef2359fd0cada53fb607d02cef
Gisela <gisequiroguita@gmail.com>
2017-05-31 00:38:01 -0300

    Proyecto

---
+++ ProyectoCine/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProyectoCine.Startup))]
+namespace ProyectoCine
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
