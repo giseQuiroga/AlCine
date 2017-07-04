891c3fb4daaaea46f8f8fc91e0e6c964f9f9d0f9
Carolina Gregorio <ccaro.-@hotmail.com>
2017-07-04 00:49:07 -0300

    Validaciones funcionando

--- ProyectoAlCine/Models/CarteleraNegocio.cs
+++ ProyectoAlCine/Models/CarteleraNegocio.cs
@@ -12,39 +12,72 @@ namespace ProyectoAlCine.Models
 
         public string validarCartelera(Cartelera cartelera)
         {
-            DateTime fecha_inicio = Convert.ToDateTime(cartelera.FechaInicio.ToShortDateString());
-            DateTime fecha_fin = Convert.ToDateTime(cartelera.FechaFin.ToShortDateString());
+            if (Convert.ToDateTime(cartelera.FechaInicio).CompareTo(Convert.ToDateTime(cartelera.FechaFin)) >= 0)
+            {
+                return "Las fechas ingresadas no son válidas.";
+            }
 
-            if (db.Carteleras.Where(a=> a.NumeroSala == cartelera.NumeroSala).Count() > 7)
+            List<Cartelera> carteleras = db.Carteleras.ToList();
+            foreach (var c in carteleras)
+            {
+                if (cartelera.IdSede == c.IdSede)
+                {
+                    if (cartelera.IdPelicula == c.IdPelicula && cartelera.IdVersion == c.IdVersion)
+                    {
+                        return "La Película ingresada ya existe en la misma Sede y con la misma Versión";
+                    }
+
+                    if (cartelera.FechaInicio >= c.FechaInicio && cartelera.FechaInicio <= c.FechaFin)
+                    {
+                        if (cartelera.HoraInicio - c.HoraInicio < 30)
+                        {
+                            return "Cada película debe tener 30 minutos de diferencia.";
+                        }
+                    }
+
+                        if (cartelera.NumeroSala == c.NumeroSala)
+                        {
+                        if (cartelera.FechaInicio >= c.FechaInicio && cartelera.FechaInicio <= c.FechaFin)
+                        {
+                            if (cartelera.Lunes == true && c.Lunes == true)
+                            {
+                                return "Los días seleccionados ya están ocupados en esta fecha";
+                            }
+                            if (cartelera.Martes == true && c.Martes == true)
+                            {
+                                return "Los días seleccionados ya están ocupados en esta fecha";
+                            }
+                            if (cartelera.Miercoles == true && c.Miercoles == true)
+                            {
+                                return "Los días seleccionados ya están ocupados en esta fecha";
+                            }
+                            if (cartelera.Jueves == true && c.Jueves == true)
+                            {
+                                return "Los días seleccionados ya están ocupados en esta fecha";
+                            }
+                            if (cartelera.Viernes == true && c.Viernes == true)
+                            {
+                                return "Los días seleccionados ya están ocupados en esta fecha";
+                            }
+                            if (cartelera.Sabado == true && c.Sabado == true)
+                            {
+                                return "Los días seleccionados ya están ocupados en esta fecha";
+                            }
+                            if (cartelera.Domingo == true && c.Domingo == true)
+                            {
+                                return "Los días seleccionados ya están ocupados en esta fecha";
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (db.Carteleras.Where(a=> a.NumeroSala == cartelera.NumeroSala && a.Sede == cartelera.Sede).Count() > 7)
             {
                 return "No puede cargar más de 7 funciones por Sala.";
             }
 
-            if (db.Carteleras.Where(b => b.HoraInicio - cartelera.HoraInicio < 30).Count() > 0)
-            {
-                return "Las funciones no pueden tener menos de 30 minutos de diferencia.";
-            }
-
-            if (db.Carteleras.Where(c => c.IdPelicula == cartelera.IdPelicula && c.IdSede == cartelera.IdSede && c.IdVersion == cartelera.IdVersion).Count() > 0)
-            {
-                return "Ya existe esta función con la misma Versión en la misma Sede.";
-            }
-
-            if (db.Carteleras.Where(d => d.FechaInicio >= fecha_inicio && d.FechaFin >= fecha_fin && d.FechaInicio <= fecha_fin).Count() > 0)
-            {
-
-                return "Ya existe una cartelera que utiliza la misma Fecha final.";
-            }
-
-            if (db.Carteleras.Where(e => e.FechaInicio <= fecha_inicio && e.FechaFin >= fecha_fin).Count() > 0)
-            {
-                return "Ya existe una cartelera en el período indicado.";
-            }
-
-            if (db.Carteleras.Where(f => f.FechaInicio <= fecha_inicio && f.FechaFin <= fecha_fin && f.FechaFin >= fecha_inicio).Count() > 0)
-            {
-                return "Ya existe una cartelera que utiliza la misma Fecha inicial.";
-            }
+            
             
             return null;
 
